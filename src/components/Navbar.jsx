86db56ec45258a214b41fd72ed5394611018de75
Sam B <samb@Sams-MacBook-Pro.local>
2022-11-11 07:04:08 -0600

    before day2homework

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+
+export default function Navbar(props) {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">Welcome {props.name} from {props.city}</a>
+                
+            </div>
+
+            
+
+        </nav>
+    )
+}
